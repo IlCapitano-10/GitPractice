bf68e7cfac9d9a996f220385634ca5db6bca00bf
Astrit <astrit989@gmail.com>
2020-05-21 16:47:40 -0500

    login feature created

--- src/main/java/cybertek/Gitepractice.java
+++ src/main/java/cybertek/Gitepractice.java
@@ -5,5 +5,6 @@ public class Gitepractice {
         //write your code here
         System.out.println("Hello World");
         System.out.println("Cybertek school");
+        System.out.println("2020");
     }
 }
---
+++ src/main/java/cybertek/Login.java
@@ -0,0 +1,7 @@
+package cybertek;
+
+public class Login {
+    public static void main(String[] args) {
+        System.out.println("login functions");
+    }
+}
