ae74b0f3caa8d5e9475eb9eb3a4544a8a01aa414
Astrit <astrit989@gmail.com>
2020-05-21 16:15:06 -0500

    update

--- src/main/java/cybertek/Gitepractice.java
+++ src/main/java/cybertek/Gitepractice.java
@@ -2,6 +2,8 @@ package cybertek;
 
 public class Gitepractice {
     public static void main(String[] args) {
+        //write your code here
         System.out.println("Hello World");
+        System.out.println("Cybertek school");
     }
 }
